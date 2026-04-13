391fbc4bf1a5938e69c667da7a8f4179e8aae9ed
Yifan Wang <yifanwang102@gmail.com>
2026-04-12 22:38:03 -0400

    indentation

--- app.py
+++ app.py
@@ -158,5 +158,5 @@ def predict():
 
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
